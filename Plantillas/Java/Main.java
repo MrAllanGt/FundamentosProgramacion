a36ab5f1d98db6f96048035c5ead97168e144552
MrAllanGt <62777790+MrAllanGt@users.noreply.github.com>
2020-05-14 22:04:03 -0600

    Update Main.java

--- Plantillas/Java/Main.java
+++ Plantillas/Java/Main.java
@@ -3,14 +3,19 @@ import java.io.InputStreamReader;
 
 class Main {
   public static void main(String[] args) {
+    // Vars / Cons + Objects
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    
+    // Inputs
     try{
 
     }catch(Exception e){
       print("Se generó la siguiente excepción: " + e.getMessage());
     }
-
+    
+    // Process
+    
+    // Outputs
   }
 
   public static void print(String s){
